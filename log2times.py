416d24b0f1807f69fb5ee1fddfece60411de1ce9
shawnxz <xu.3.zhang@uconn.edu>
2018-10-16 16:46:58 -0400

    xzhw7

--- log2times.py
+++ log2times.py
@@ -2,20 +2,14 @@
 import pandas as pd
 import numpy as np
 
-# read the log file
-df = pd.read_table('localizer.txt')
-# adjust the times
-df['Time'] = df['Time'] - df.iloc[0]['Duration'] - df.iloc[0]['Time']
+# read the log file (change the path)
+df = pd.read_table('/scratch/psyc5171/dataset1/task-fingerfootlips_events.tsv')
 
-# remove non condition codes
-df = df[~df['Code'].isin(['fix', 'rest', '4', '8', 'checker_flash', 'wait'])]
-# convert to seconds
-df['Time'] = df['Time']/10000.0
-
-groups = df.groupby(df['Code'])
-times = groups['Time'].unique()
+groups = df.groupby(df['trial_type'])
+times = groups['onset'].unique()
 
 # save times
 for condition in times.index:
 	onsets = np.array(times[condition])
 	np.savetxt(condition + '.txt', onsets[np.newaxis], fmt='%0.2f')
+
