ca252fe3a618c47d33817e55bc886ae7e293220f
shawnxz <xu.3.zhang@uconn.edu>
2018-12-03 20:00:15 -0500

    hw7

--- ap.sh
+++ ap.sh
@@ -4,39 +4,40 @@
 # creation date: Mon Oct  1 23:54:19 2018
 
 # set subject and group identifiers
-set subj  = sub-02
-set gname = test
-set session = test
+set subj  = $argv[1]
+set gname = $argv[2]
+set session = $argv[2]
 
 # set data directories
-set top_dir = /scratch/psyc5171/dataset1/sub-${subj}_ses-${session}
+set top_dir = /scratch/psyc5171/dataset1/sub-${subj}/ses-${session}
 set anat_dir  = $top_dir/anat
 set epi_dir   = $top_dir/func
 set stim_dir  = $top_dir/onsets
 
 # run afni_proc.py to create a single subject processing script
-afni_proc.py -subj_id $subj                                                  \
-        -script run_afni.sh -scr_overwrite                                    \
+afni_proc.py -subj_id ${subj}-${session}                                     \
+        -script run_afni_${subj}-${session}.sh -scr_overwrite                \
         -blocks tshift align tlrc volreg blur mask scale regress             \
-        -copy_anat $anat_dir/sub-${subj}_ses-${session}_T1w.nii.gz                          \
+        -copy_anat $anat_dir/sub-${subj}_ses-${session}_T1w.nii.gz           \
         -dsets $epi_dir/sub-${subj}_ses-${session}_task-fingerfootlips_bold.nii.gz            \
-        -tcat_remove_first_trs 0   -tshift_opts_ts -tpattern alt+z          \
+        -tcat_remove_first_trs 2                                             \
+        -tshift_opts_ts -tpattern alt+z                                      \
         -tlrc_base MNI_avg152T1+tlrc                                         \
         -volreg_align_to first                                               \
         -volreg_align_e2a                                                    \
         -volreg_tlrc_warp                                                    \
         -blur_size 6.0                                                       \
-        -regress_stim_times /scratch/psyc5171/xuz17008/hw7/*.txt  \
+        -regress_stim_times /scratch/psyc5171/xuz17008/hw7/*.txt             \
         -regress_stim_labels                                                 \
-            finger foot lips                                         \
-        -regress_basis 'BLOCK(15)'                                                 \
+            finger foot lips                                                 \
+        -regress_basis 'BLOCK(15)'                                           \
         -regress_censor_motion 0.5                                           \
         -regress_apply_mot_types demean deriv                                \
         -regress_motion_per_run                                              \
         -regress_opts_3dD                                                    \
             -gltsym 'SYM: finger -foot' -glt_label 1 finger-foot             \
             -gltsym 'SYM: finger -lips' -glt_label 2 finger-lips             \
-            -gltsym 'SYM: foot -lips' -glt_label 3 foot-lips             \
+            -gltsym 'SYM: foot -lips' -glt_label 3 foot-lips                 \
         -regress_make_ideal_sum sum_ideal.1D                                 \
         -regress_est_blur_epits                                              \
         -regress_est_blur_errts                                              \
--- sbatch_ap.sh
+++ sbatch_ap.sh
@@ -3,14 +3,27 @@
 #SBATCH --mail-user=xu.3.zhang@uconn.edu	# Your email address
 #SBATCH --ntasks=1				# Run a single serial task
 #SBATCH --cpus-per-task=1     # Number of cores to use
-#SBATCH --mem=2048mb				# Memory limit
-#SBATCH --time=0:10:00				# Time limit hh:mm:ss
-#SBATCH -e error_ap.log				# Standard error
-#SBATCH -o output_ap.log				# Standard output
+#SBATCH --mem=4096mb				# Memory limit
+#SBATCH --time=1:00:00				# Time limit hh:mm:ss
+#SBATCH -e error_ap_%a.log				# Standard error
+#SBATCH -o output_ap_%a.log				# Standard output
 #SBATCH --job-name=ap			# Descriptive job name
 #SBATCH --partition=serial
+#SBATCH --array=1-10
 module load singularity
 
 # run the afni_proc.py script
-cd /scratch/psyc5171/xuz17008
-singularity run /scratch/psyc5171/containers/burc-lite.img /scratch/psyc5171/xuz17008/ap.sh
+cd /scratch/psyc5171/xuz17008/hw7/
+singularity run /scratch/psyc5171/containers/burc-lite.img /scratch/psyc5171/xuz17008/hw7/ap.sh
+
+subj=`printf "%02d" $SLURM_ARRAY_TASK_ID`
+
+# run the afni_proc.py script
+cd /scratch/psyc5171/xuz17008/hw7
+for session in test retest; do
+	singularity run /scratch/psyc5171/containers/burc-lite.img \
+	/scratch/psyc5171/xuz17008/hw7/ap.sh $subj $session
+	singularity run /scratch/psyc5171/containers/burc-lite.img \
+	/scratch/psyc5171/xuz17008/hw7/run_afni_${subj}-${session}.sh
+done
+	
